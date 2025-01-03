1d83f41d3e471e3d17d8aa471b726ecde13bd361
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-01-03 16:50:25 -0600

    Fixed undraggable section on right, smoothstep

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -24,7 +24,7 @@ const initialNodes = [
   { id: '1', type: 'RootNode', position: { x: 0, y: 0 },},
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' }},
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2',}];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2', animated :true, type: 'smoothstep'}];
 
 
  
