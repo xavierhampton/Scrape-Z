59ff40a1b919d3396673a13b3d8678bde588421a
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-01-20 23:16:24 -0600

    added main python entry

---
+++ python/main.py
@@ -0,0 +1,15 @@
+#Imports
+from bs4 import BeautifulSoup
+
+#Main
+class Main():
+    def __init__(self):
+        print("Starting")
+        self.run()
+
+    def run(self):
+        print("Running")
+    
+#Checks if running from Main
+if __name__ == "__main__":
+    Main()
